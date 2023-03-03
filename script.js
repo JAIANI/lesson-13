01165a17dec15dbbffb8b1b0eaea33643d3484f0
ბექა ჯაიანი <121308857+JAIANI@users.noreply.github.com>
2023-03-03 11:22:33 +0300

    about Me

---
+++ script.js
@@ -0,0 +1,12 @@
+let myName = "Beqa";
+let myHoby = ["reading books", "fitnes", " play footbal", "hunting"];
+let yearOfBirth = 2005;
+let currentYear = 2023;
+
+let aboutMe = `My name is ${myName}, I am ${
+  currentYear - yearOfBirth
+} years old and my hoby is ${myHoby[1]}, ${myHoby[2]}, ${myHoby[0]} and ${
+  myHoby[3]
+}`;
+
+console.log(aboutMe);
